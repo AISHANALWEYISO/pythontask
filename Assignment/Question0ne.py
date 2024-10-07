e77413d1639ad4a1751a5bccb9d0dbf823e7c0de
AISHANALWEYISO <AISHANALWEYISO>
2024-10-07 14:35:57 +0300

    exercise

---
+++ Assignment/Question0ne.py
@@ -0,0 +1,9 @@
+#Question one
+#The volume of a sphere with radius r is (4/3)*pie*r**2.
+#What is the volume of the sphere with radius 5.
+#make sure the program enters the radius from the keyboard and provide the answer in 2 decimal places
+r=int(input("Enter the radius : "))
+pie=3.14
+volume=(4/3)*pie*r**2
+
+print(f"The volume of the sphere is {volume:.2f}")
---
+++ Assignment/QuestionTwo.py
@@ -0,0 +1,7 @@
+#Question two
+#create a progaram to calculate the area of a triangle (1/2*base*height)
+#Base and height should be entered using the keyboard.
+base=int(input("Enter the base: "))
+height=int(input("Enter the height: "))
+Area=1/2*base*height
+print(f"Area = {Area:.2f}")
